9bec5128d6adfe2b09f0a6f75ee728918f80c294
AlexT <acc.tung@gmail.com>
2021-12-13 04:18:26 -0500

    rest routes functionality completed, added read.me

---
+++ models/item.js
@@ -0,0 +1,15 @@
+const mongoose = require ('mongoose')
+
+const itemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  itemDate: {
+    type: Date,
+    required: true,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('Item', itemSchema)
--- routes/items.js
+++ routes/items.js
@@ -1,24 +1,69 @@
 const express = require('express')
 const router = express.Router()
+const Item = require('../models/item')
 
-router.get('/', (req, res) => {
-  res.send('Hello World')
+let getItem = async (req, res, next) => {
+  let item
+  try{
+    item = await Item.findById(req.params.id)
+    if (item == null) {
+      return res.status(404).json({ message: 'Cannot find item'})
+    }
+  } catch (err) {
+    return res.status(500).json({message: err.message})
+  }
+  res.item = item
+  next()
+}
+
+router.get('/', async (req, res) => {
+  try {
+    const items = await Item.find()
+    res.json(items)
+  } catch (err) {
+    res.status(500).json({message: err.message})
+  }
 })
 
-router.get('/:id', (req, res) => {
-  res.send(req.params.id)
+router.get('/:id', getItem, (req, res) => {
+  res.json(res.item)
 })
 
-router.post('/', (req, res) => {
-  
+router.post('/', async (req, res) => {
+  const item = new Item({
+    name: req.body.name
+  })
+  try {
+    const newItem = await item.save()
+    res.status(201).json(newItem)
+    // Status 200 means things were successful, 201 means an item was created successfully
+  } catch (err) {
+    res.status(400).json({message: err.message})
+    // status 400 is for bad user input
+  }
 })
 
-router.patch('/:id', (req, res) => {
-  
+router.patch('/:id', getItem, async (req, res) => {
+  if (req.body.name !== null) {
+    res.item.name = req.body.name
+  }
+  // add other checks here when other properties get added to Item schema
+  try {
+    const updatedItem = await res.item.save()
+    res.json(updatedItem)
+  } catch (err) {
+    res.status(400).json({message: err.message})
+  }
 })
 
-router.delete('/:id', (req, res) => {
-  
+router.delete('/:id', getItem, async (req, res) => {
+  try {
+    await res.item.remove()
+    res.json({message: 'deleted item'})
+  } catch (err) {
+    res.status(500).json({message: err.message})
+  }
 })
+
 
 module.exports = router
